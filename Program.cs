cb5031d26743172a5285177152c0797728d22451
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-06 02:48:50 +0630

    -

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors();
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<UserManagementDBContext>(
-    options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")); },
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")),
     ServiceLifetime.Transient, ServiceLifetime.Transient);
 
 var app = builder.Build();
